884b2aec2ee9564020fb68696ad69f4915b4729e
Andrzej <anjeynov@gmail.com>
2023-11-13 10:00:49 +0100

    add migrations for user and product

---
+++ Sources/App/Migrations/CreateProduct.swift
@@ -0,0 +1,23 @@
+import Fluent
+
+struct CreateProduct: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema(Product.schema)
+            .id()
+            .field(Product.CodingKeys.title.fieldKey, .string, .required)
+            .field(Product.CodingKeys.description.fieldKey, .string, .required)
+            .field(Product.CodingKeys.isDeleted.fieldKey, .bool, .required)
+            .field(Product.CodingKeys.creationDate.fieldKey, .date, .required)
+            .field(
+                Product.CodingKeys.creatorUserId.fieldKey,
+                .uuid, 
+                .required,
+                .references(User.schema, "id")
+            )
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema(Product.schema).delete()
+    }
+}
---
+++ Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,17 @@
+import Fluent
+
+struct CreateUser: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema(User.schema)
+          .id()
+          .field(User.CodingKeys.name.fieldKey, .string, .required)
+          .field(User.CodingKeys.username.fieldKey, .string, .required)
+          .field(User.CodingKeys.password.fieldKey, .string, .required)
+          .unique(on: User.CodingKeys.username.fieldKey)
+          .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema(User.schema).delete()
+    }
+}
